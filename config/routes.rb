c1288b9b98e75269c2405555946137a70c7f63d9
benny <benny08118@gmail.com>
2021-01-29 22:00:32 +0800

    add backend system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'products/index'
+  end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products,only:[:index,:show]
+
+  namespace :admin do
+    root 'products#index'
+    resources :products
+  end
 end
