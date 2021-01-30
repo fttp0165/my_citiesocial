1438267a7bfba99cbc31432353d988cb6ee1f340
benny <benny08118@gmail.com>
2021-01-30 23:21:26 +0800

    add friendly_id

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'omniauth-google-oauth2', '~> 0.8.1'
 gem 'figaro', '~> 1.2'
 gem 'omniauth', '~> 2.0', '>= 2.0.1'
 gem "omniauth-rails_csrf_protection"
+gem 'friendly_id', '~> 5.4', '>= 5.4.2'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,19 @@
 class Product < ApplicationRecord
+  extend FriendlyId
+  friendly_id :code_generator, use: :slugged,slug_column: :code
   acts_as_paranoid
-  belongs_to :vendor
+ 
   validates :name, presence: true
-  validates :code, presence: true
+  validates :list_price,:sell_price,numericality:{greater_than:0,allow_nil:true}
+  validates :code, uniqueness: true
+
+  belongs_to :vendor
+
+
+  private
+
+  def code_generator
+     SecureRandom.hex(10)  
+  end
   
 end
